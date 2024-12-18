14604532e105135f03ed0791ab918311fc98d900
RanaUniverse <RanaUniverse321@gmail.com>
2024-12-18 14:01:57 +0530

    Php in browser

---
+++ README.md
@@ -0,0 +1,8 @@
+# This is Learnin from different website online
+I will just check how it works, 
+
+# How to start the  php to check
+
+``` php -S 0.0.0.0:9999 ```
+
+if i run this in the folder's terminal then it will give me a localhost:9999 in browser to check how this will work.
---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+// Here i will keep the php logic
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php Learning from online </title>
+</head>
+
+<body>
+    <h1>This is PHP Learning By Rana Universe </h1>
+</body>
+
+</html>
